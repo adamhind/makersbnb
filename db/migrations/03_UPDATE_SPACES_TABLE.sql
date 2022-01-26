b3a9dead7f157e447e7577e639e8f0e087af89cd
Adam Hindmarsh <adam_hindmarsh@hotmail.com>
2022-01-26 13:42:34 +0000

    updated spaces table

--- db/migrations/03_UPDATE_SPACES_TABLE.sql
+++ db/migrations/03_UPDATE_SPACES_TABLE.sql
@@ -1,3 +1,5 @@
 ALTER TABLE spaces ADD COLUMN owner_id INT references users(user_id);
 ALTER TABLE spaces RENAME COLUMN id TO space_id;
 ALTER TABLE spaces RENAME COLUMN name TO space_name;
+ALTER TABLE spaces ADD COLUMN available_from DATE;
+ALTER TABLE spaces ADD COLUMN available_to DATE;
