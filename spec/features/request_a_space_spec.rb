a0ee6e58e579434240fb3606e510b5c076f4fb0b
MichaelBodendorf <bodi@gmx.at>
2022-01-28 12:08:12 +0100

    created booking class with .add method for user story 3/7

--- spec/features/request_a_space_spec.rb
+++ spec/features/request_a_space_spec.rb
@@ -1,7 +1,11 @@
 feature 'request a space' do 
   scenario "A booker is able to click to request a space" do 
-    visit ('/spaces') 
-    click_button('Request Test Space #1')
-    expect(page).to have_content "Thank you for your booking request for Test Space #1. We will be in touch soon."
+    visit ('/home')
+    fill_in('Check-in', with: '2022-02-21')
+    fill_in('Check-out', with: '2022-02-27')
+    click_button('Search')
+    click_button('Request Test Space #3')
+    expect(page).to have_content "Thank you for your booking request for Test Space #3. We will be in touch soon."
   end 
+
 end 
